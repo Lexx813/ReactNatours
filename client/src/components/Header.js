757863bf206c4fd29bcaf344bb86684e786fcd3c
Luis Quinones <alexx813@gmail.com>
2018-10-08 16:46:02 -0500

    added BEM

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,15 @@ import LOGO from "../img/logo-white.png";
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={LOGO} alt="Logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={LOGO} alt="Logo" className="header__logo" />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
-        <a href="" className="btn btn-white btn-animated">
+        <a href="" className="btn btn--white btn--animated">
           Discover our tours
         </a>
       </div>
