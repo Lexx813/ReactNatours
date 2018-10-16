7d7e458fc2f12526ff8d6ed58c13962b8d3934b5
Luis Quinones <alexx813@gmail.com>
2018-10-16 06:53:06 -0500

    added cards

--- client/src/components/Tours.js
+++ client/src/components/Tours.js
@@ -19,7 +19,15 @@ const Tours = () => {
                   The Sea Explorer
                 </span>
               </h4>
-              <div className="card__details">details</div>
+              <div className="card__details">
+                <ul>
+                  <li>3 day tours</li>
+                  <li>up to 30 people</li>
+                  <li>2 tour guides</li>
+                  <li>Sleep in cozy hotels </li>
+                  <li>Difficulty: easy</li>
+                </ul>
+              </div>
             </div>
             <div className="card__side card__side--back card__side--back-1">
               Back
